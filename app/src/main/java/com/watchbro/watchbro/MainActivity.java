c0079a687fa4e6f5e5f9dc466f121cb46fb01dde
Nebuchadnezzard <pierre.couderc.pro@gmail.com>
2018-03-08 16:56:45 +0100

    commencement firebase

--- app/src/main/java/com/watchbro/watchbro/MainActivity.java
+++ app/src/main/java/com/watchbro/watchbro/MainActivity.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -40,6 +42,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        FirebaseDatabase dbFire = FirebaseDatabase.getInstance();
+
+        GoogleSignin
     }
 
     @Override
